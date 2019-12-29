3fbaa43fecd7938dd50df17e12145e6f2abbab94
ivanfslee <ivanfslee@gmail.com>
2019-12-29 13:38:38 -0800

    Added route handling for express server for production deployment

--- index.js
+++ index.js
@@ -49,6 +49,25 @@ require('./routes/authRoutes')(app); //require authRoutes.js will be a function,
 //here we are importing the express routes we set up in billingroutes.js - which results in a function, then we pass in 'app' which is our express app obj as an argument 
 require('./routes/billingRoutes')(app); //this is done in lesson 104
 
+//**the following code should only run when in production environment (e.g. deploying to heroku)
+if (process.env.NODE_ENV === 'production') { //process.env.NODE_ENV is environment variable automatically set up by Heroku
+    // Express will serve up production assets
+    // like our main.js file, or main.css file 
+    app.use(express.static('client/build')); //if any get request comes in for some route or some file or anything we do not understand what it is looking for - then look in 'client/build' and see if that file is there. if it is, serve up that file 
+
+    // Express will serve up the index.html file 
+    // if it doesn't recognize the route
+    const path = require('path'); //we require in a package called 'path'
+    app.get('*', (req, res) => { //you can think about this get request as a catchall 
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    }) //this block of code is saying - if someone makes a request for a route we do not understand, just serve up the index.html document
+    //if we don't know what the route is, we will assume that the react router side of our application is responsible for the route - so we kick the user over to the client side of our application 
+    //this block also is after the authRoutes and billingRoutes on line 47 and 50. If the requested route is not in line 47/50 or in client/build (line 56) - only then do we serve up the index.html file 
+}
+
+//**
+
+
 const PORT = process.env.PORT || 5000; //capitalized to show it shouldn't change, heroku assigns a port - using process.env.PORT - 5000 is used in our development environment 
 
 //express tells node to listen to port 5000
